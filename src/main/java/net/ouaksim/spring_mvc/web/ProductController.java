86101519327ead5aadbf6ddcec23e1cb7f96a389
Halima <halimaouaksim@gmail.com>
2026-03-09 23:20:13 +0000

    Affichier la liste des produits avec thymeleaf

--- src/main/java/net/ouaksim/spring_mvc/web/ProductController.java
+++ src/main/java/net/ouaksim/spring_mvc/web/ProductController.java
@@ -1,13 +1,17 @@
 package net.ouaksim.spring_mvc.web;
 
 import net.ouaksim.spring_mvc.SpringMvcApplication;
+import net.ouaksim.spring_mvc.entities.Product;
 import net.ouaksim.spring_mvc.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -15,7 +19,9 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("productList", products);
         return "products";
 
     }
